e15ac75bfae73b90e6aa36904f3efa19dbe34b0f
Jameson Taylor <jameson@ionic.io>
2022-01-24 11:43:29 -0500

    Add secure-storage;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-enterprise/secure-storage/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  private database: SQLiteObject;
+
+  constructor(private sqlite: SQLite) {
+    this.initializeDatabase();
+  }
+
+  private async initializeDatabase() {
+    try {
+      const db = await this.sqlite.create({
+        name: "mydb",
+        location: "default",
+        key: "password"
+      });
+
+      this.database = db;
+
+      await db.executeSql('CREATE TABLE IF NOT EXISTS software(name, company, type, version)', [])
+    } catch (e) {
+      console.error('Unable to initialize database', e);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { SQLite } from '@ionic-enterprise/secure-storage/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
